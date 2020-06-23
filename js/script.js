5d865110c8ee296edc8b4ed48b40a420da01dfb2
Jason Climo <jason@Jasons-MBP.hub>
2020-06-23 18:39:27 +1200

    Made the new functions etc

--- js/script.js
+++ js/script.js
@@ -4,33 +4,51 @@
 
 
 
-$("#buttonSubmit").click(function(){
-    var ammountOfPeople = document.getElementById("peopleInput").value;
-    console.log(ammountOfPeople)
-
-    var data = {
-        // Array data in order minimum people, max people, price, minNight, maxNight
-        hotel1: [1, 2, 157, 1, 5],
-        hostel1: [1, 1, 30, 1, 10],
-        motel1: [2, 4, 90, 3, 10],
-        house: [1, 4, 240, 2, 15]
-    };
+$('#buttonSubmit').click(function () {
+  // Global Variables
+  var ammountOfPeople = document.getElementById('peopleInput').value
+  var nightsToStay = document.getElementById('nightsToStay').value
 
 
+  var data = {
+    // Array data in order minimum people, max people, price, minNight, maxNight
+    hotel1: [1, 2, 157, 1, 5],
+    hostel1: [1, 1, 30, 1, 10],
+    motel1: [2, 4, 90, 3, 10],
+    house: [1, 4, 240, 2, 15]
+  }
+
+  function calaulateAmmontOfPeople (data) {
+    var ammountOfPeopleData = {
+      hotel1: false,
+      hostel1: false,
+      motel1: false,
+      house: false
+    }
     // Find out if the ammount of people matches the following
-    if (ammountOfPeople <= data.hotel1[0] && ammountOfPeople >= data.hotel1[1]) {
-        console.log("dhsakj");
-    } else {
-        console.log("Something is wrong")
-    };
+    if (ammountOfPeople >= data.hotel1[0] && ammountOfPeople <= data.hotel1[1]) {
+      ammountOfPeopleData.hotel1 = true
+      console.log(ammountOfPeopleData.hotel1)
+    }
 
+    if (ammountOfPeople >= data.hostel1[0] && ammountOfPeople <= data.hostel1[1]) {
+      ammountOfPeopleData.hostel1 = true
+      console.log(ammountOfPeopleData.hostel1)
+    }
 
+    if (ammountOfPeople >= data.motel1[0] && ammountOfPeople <= data.motel1[1]) {
+      ammountOfPeopleData.motel1 = true
+      console.log(ammountOfPeopleData.motel1)
+    }
 
+    if (ammountOfPeople >= data.house[0] && ammountOfPeople <= data.house[1]) {
+      ammountOfPeopleData.house = true
+      console.log(ammountOfPeopleData.house)
+    }
 
-    // Call the functions
-    // howManyPeople(data, ammountOfPeople);
+    return ammountOfPeopleData;
+  }
 
+  calaulateAmmontOfPeople(data)
 
-
-
-});
+})
